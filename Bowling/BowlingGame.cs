ac25951039e445b5be0afca4d7a20d66fa6ae037
Momchil Anachkov <momchil.anachkov@gmail.com>
2020-04-02 03:13:54 +0300

    pull bonus rolls out of the last frame

--- Bowling/BowlingGame.cs
+++ Bowling/BowlingGame.cs
@@ -7,35 +7,36 @@ namespace Bowling
     public class BowlingGame
     {
         private readonly List<Frame> _finishedFrames = new List<Frame>();
-        private Frame _currentFrame = new Frame(false);
+        private readonly List<int> _bonusRolls = new List<int>();
+        private Frame _currentFrame = new Frame();
 
         public void Roll(byte knockedPins)
         {
             if (this.Finished) throw new InvalidOperationException();
-            this._currentFrame.Roll(knockedPins);
+            if (!this.RegularRollsAreFinished)
+                this._currentFrame.Roll(knockedPins);
+            else
+                this._bonusRolls.Add(knockedPins);
             if (this._currentFrame.Finished) this.FinalizeFrame();
         }
+        
+        public bool Finished =>
+            this._finishedFrames.Count == 10 && this._finishedFrames.Last().RollSum < 10 ||
+            this._finishedFrames.Count == 10 && this._finishedFrames.Last().IsSpare && this._bonusRolls.Count == 1 ||
+            this._finishedFrames.Count == 10 && this._finishedFrames.Last().IsStrike && this._bonusRolls.Count == 2;
 
         private void FinalizeFrame()
         {
             this._finishedFrames.Add(this._currentFrame);
-            this._currentFrame = new Frame(this.IsLastFrame);
+            this._currentFrame = new Frame();
         }
         
-        private bool IsLastFrame => this._finishedFrames.Count == 9;
-
-        public bool Finished => this._finishedFrames.Count == 10;
+        private bool RegularRollsAreFinished => this._finishedFrames.Count == 10;
 
         private class Frame
         {
-            private readonly bool _isLastFrame;
             private byte _pinsLeft = 10;
             private List<int> _rolls = new List<int>();
-
-            public Frame(bool isLastFrame)
-            {
-                this._isLastFrame = isLastFrame;
-            }
 
             public void Roll(byte knockedPins)
             {
@@ -44,18 +45,16 @@ namespace Bowling
 
                 this._rolls.Add(knockedPins);
                 this._pinsLeft -= knockedPins;
-                if (this._pinsLeft == 0  && this._isLastFrame && this._rolls.Count < 3) this._pinsLeft = 10;
             }
 
             public bool Finished =>
-                (this.RollSum < 10 && this._rolls.Count == 2) ||
-                (this.RollSum == 10 && !this._isLastFrame) ||
-                (this.RollSum > 10 && this._isLastFrame && this._rolls.Count == 3);
+                this._pinsLeft == 0 || this._rolls.Count == 2;
 
-            private int RollSum
-            {
-                get { return this._rolls.Sum(); }
-            }
+            public int RollSum => this._rolls.Sum();
+
+            public bool IsStrike => this._pinsLeft == 0 && this._rolls.Count == 1;
+            
+            public bool IsSpare => this._pinsLeft == 0 && this._rolls.Count == 2;
         }
     }
 }
