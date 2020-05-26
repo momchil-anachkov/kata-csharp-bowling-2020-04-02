b44f1b785bf8a5b0eb8816ff486cd35407b9c0f9
Momchil Anachkov <momchil.anachkov@gmail.com>
2020-05-27 01:41:20 +0300

    scoring is more readable now

--- Bowling/BowlingScore.cs
+++ Bowling/BowlingScore.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Bowling
 {
@@ -10,31 +12,35 @@ namespace Bowling
             var finishedFrames = game.FinishedFrames;
             for (var i = 0; i < finishedFrames.Count(); i++)
             {
-                var frameScore = finishedFrames[i].Rolls.Sum();
-                if (i == finishedFrames.Count() - 1)
-                {
+                var currentFrame = finishedFrames[i];
+                var frameScore = currentFrame.Rolls.Sum();
+                if (currentFrameIsLastFrame(i, finishedFrames))
                     frameScore += game.BonusRolls.Sum();
-                }
                 else
-                {
-                    if (finishedFrames[i].IsSpare)
-                    {
-                        frameScore += finishedFrames[i + 1].Rolls[0];
-                    }
-                    else if (finishedFrames[i].IsStrike)
-                    {
-                        var nextFrame = finishedFrames[i + 1];
-                        var nextNextFrame = finishedFrames[i + 2];
-                        frameScore += nextFrame.Rolls[0];
-                        if (nextFrame.IsStrike) frameScore += nextNextFrame.Rolls[0];
-                        else frameScore += nextFrame.Rolls[1];
-                    }
-                }
-
+                    if (currentFrame.IsSpare) frameScore += nextRoll(i, finishedFrames);
+                    else if (currentFrame.IsStrike) frameScore += nextTwoRolls(i, finishedFrames);
                 score += frameScore;
             }
 
             return score;
+        }
+
+        private bool currentFrameIsLastFrame(int i, List<BowlingFrame> finishedFrames)
+        {
+            return i == finishedFrames.Count() - 1;
+        }
+
+        private int nextRoll(int i, List<BowlingFrame> frames)
+        {
+            var nextFrame = frames[i + 1];
+            return nextFrame.Rolls[0];
+        }
+
+        private int nextTwoRolls(int i, List<BowlingFrame> frames)
+        {
+            var nextFrame = frames[i + 1];
+            var nextNextFrame = frames[i + 2];
+            return nextFrame.Rolls[0] + (nextFrame.IsStrike ? nextNextFrame.Rolls[0] : nextFrame.Rolls[1]);
         }
     }
 }
